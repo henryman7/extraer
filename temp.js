4283b8716a7d1cbdcca587ee20d96d5d1af609e9
henryman7 <157765159+henryman7@users.noreply.github.com>
2025-06-16 15:09:50 -0600

    Create temp.js

---
+++ temp.js
@@ -0,0 +1,1 @@
+{"expand":"renderedFields,names,schema,operations,editmeta,changelog,versionedRepresentations","id":"11874536","self":"https://jira.globaldevtools.bbva.com/rest/api/2/issue/11874536","k
