6a9e2f33efc0b7fe6b6b5efdd828c912034c3363
henryman7 <157765159+henryman7@users.noreply.github.com>
2025-06-18 08:26:03 -0600

    Update General.js

--- General.js
+++ General.js
@@ -1,13 +1,3 @@
-// Función para obtener el usuario actual sin el dominio
 function getCurrentUser() {
-  const email = Session.getActiveUser().getEmail();
-  const currentUser = email.replace("@bbva.com", ""); // Eliminar el dominio
-  return currentUser;
-}
-
-// Función para obtener el ID del issue basado en el usuario y el issueKey
-function getIssueId(issueKey) {
-  const currentUser = getCurrentUser(); // Obtener el usuario actual
-  // Simulación de un ID basado en el issueKey y el usuario
-  return `ID-${issueKey}-${currentUser}`;
+  return Session.getActiveUser().getEmail();
 }
